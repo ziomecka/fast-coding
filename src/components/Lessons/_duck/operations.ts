21de3ed96db5cec9ec085e729d470955ff0eac96
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-23 15:46:16 +0100

    console.log removed

--- src/components/Lessons/_duck/operations.ts
+++ src/components/Lessons/_duck/operations.ts
@@ -8,11 +8,7 @@ import { LessonData } from '../../Lesson/_duck/reducers';
 
 export const onOpenRandomLesson = (lesson: LessonData): any => (
     async (dispatch: Dispatch ) => {
-        console.log("lesson");
-        console.log(lesson);
         const response = await dispatch(updateLesson(lesson));
-        console.log('response')
-        console.log(response)
         if (response) {
             return dispatch(turnOnTextGenerator());
         }
