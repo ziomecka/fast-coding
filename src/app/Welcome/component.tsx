9533939c6cdbc45fab9fc7fe3e6ee56c73133358
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-24 20:51:22 +0100

    Welcome. Formatting, 4 spaces

--- src/app/Welcome/component.tsx
+++ src/app/Welcome/component.tsx
@@ -13,68 +13,68 @@ import styles from './styles';
 require('./style.sass');
 
 class WelcomeComponent extends React.Component<WelcomeProps> {
-  style: CSSProperties;
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidUpdate(prevProps: WelcomeProps) {
-    const { appLocation } = this.props;
-    const prevAppLocation = prevProps.appLocation;
-
-    if (appLocation !== prevAppLocation) {
-      this.props.changeLocation(appLocation);
-    }
-  }
-
-  heading = () => {
-    const { animated, heading, classes } = this.props;
-
-    if (animated) {
-      const lastSpace = heading.lastIndexOf(' ');
-      const lastWord = heading.slice(lastSpace);
-      const remainingHeading = heading.slice(0, lastSpace);
-
-      return (
-        <>
-          {remainingHeading} {
-            Array.from(lastWord).map((letter, ind) => (
-              <span
-                className={`${classes.fallingLetters} title-falling`}
-                key={`${ind}-${letter}`}
-              >
-                {letter}
-              </span>
-            ))
-          }
-        </>
-      );
+    style: CSSProperties;
+        constructor(props) {
+        super(props);
     }
 
-    return <>{heading}</>;
-  };
+    componentDidUpdate(prevProps: WelcomeProps) {
+        const { appLocation } = this.props;
+        const prevAppLocation = prevProps.appLocation;
 
-  render()  {
-    const { classes, location } = this.props;
-    const { heading } = this;
-    const isHome = location.pathname === '/';
+        if (appLocation !== prevAppLocation) {
+            this.props.changeLocation(appLocation);
+        }
+    }
 
-    return (
-        <Paper className={
-            `${classes.welcomePaper} ${isHome ? classes.welcomeHome : classes.welcomeOther}`
-        }>
-          <h1>{heading()}</h1>
-            {isHome && (
-                <Button
-                    onClick={() => {}}
-                    className={classes.welcomeButton}
-                >
-                    Start typing
-                </Button>
-            )}
-        </Paper>
-    );
-  }
+    heading = () => {
+        const { animated, heading, classes } = this.props;
+
+        if (animated) {
+            const lastSpace = heading.lastIndexOf(' ');
+            const lastWord = heading.slice(lastSpace);
+            const remainingHeading = heading.slice(0, lastSpace);
+
+            return (
+                <>
+                    {remainingHeading} {
+                        Array.from(lastWord).map((letter, ind) => (
+                            <span
+                                className={`${classes.fallingLetters} title-falling`}
+                                key={`${ind}-${letter}`}
+                            >
+                                {letter}
+                            </span>
+                        ))
+                    }
+                </>
+            );
+        }
+
+        return <>{heading}</>;
+    };
+
+    render()  {
+        const { classes, location } = this.props;
+        const { heading } = this;
+        const isHome = location.pathname === '/';
+
+        return (
+            <Paper className={
+                `${classes.welcomePaper} ${isHome ? classes.welcomeHome : classes.welcomeOther}`
+            }>
+                <h1>{heading()}</h1>
+                    {isHome && (
+                        <Button
+                            onClick={() => {}}
+                            className={classes.welcomeButton}
+                        >
+                            Start typing
+                        </Button>
+                    )}
+            </Paper>
+        );
+    }
 };
 
 export default withStyles(styles)(WelcomeComponent);
