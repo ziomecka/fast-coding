5e23de870d63dce99336ba4b3040f61f678f7936
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-24 12:52:33 +0100

    url, shared, functins for building url with params

---
+++ src/shared/url.tsx
@@ -0,0 +1,27 @@
+export const buildParam = (paramName: string, param: string) => `?${paramName}=${param.toLowerCase()}`;
+
+export const buildParamsFromArray = (paramName:string, params: string[]): string => {
+    let result = '';
+    params.forEach(param => result += buildParam(paramName, param));
+    return result;
+};
+
+export const buildUrl = (params: URLParamsI, url: string): string => {
+    Object.keys(params).forEach(paramName => {
+        let _params = params[paramName];
+
+        if (Array.isArray(_params)) {
+            url += buildParamsFromArray(paramName, _params as string[])
+        } else {
+            url += buildParam(paramName, _params);
+        }
+
+        _params = null // GC?
+    });
+
+    return url;
+};
+
+export interface URLParamsI {
+    [paramName: string]: string | string[]
+};
