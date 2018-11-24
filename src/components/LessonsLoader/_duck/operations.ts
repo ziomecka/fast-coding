aa3ed96f0f6dddbce85d072e72fa53d328d4869d
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-24 12:53:46 +0100

    LessonsLoader. Params for url in arguments

--- src/components/LessonsLoader/_duck/operations.ts
+++ src/components/LessonsLoader/_duck/operations.ts
@@ -1,19 +1,30 @@
 import { Dispatch } from 'redux';
 import { onLoadData } from '../../../app/CSR/_duck/operations';
+import * as urlFuncs from '../../../shared/url';
+import { URLParamsI } from '../../../shared/url';
+
+const { buildUrl } = urlFuncs;
 
 import { ApplicationContainers, ComponentsContainers, LocalStorageItemTypes } from '../../../_common';
 
 const { components  } = ApplicationContainers;
 const { lessonsLoader } = ComponentsContainers;
 
-const url = '/lessons/get';
+const _url = '/lessons/get';
+const defaultParamName = 'login';
+const defaultParam = 'basic_user';
 
-export const onLoadLessons = (): any => (dispatch: Dispatch) => {
+const defaultParamNames = {
+    [defaultParamName]: defaultParam
+};
+
+export const onLoadLessons = (params: URLParamsI = defaultParamNames, url: string = _url, ): any => (dispatch: Dispatch) => {
     const applicationContainer = components;
     const container = lessonsLoader;
     const lsItem = LocalStorageItemTypes.lessons;
-    dispatch(onLoadData(url, applicationContainer, container, lsItem));
-}
+
+    dispatch(onLoadData(buildUrl(params, url), applicationContainer, container, lsItem));
+};
 
 export default {
     onLoadLessons
