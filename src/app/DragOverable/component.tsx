3507de386e4f27de539c73928866170ddaaa553b
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-25 16:18:10 +0100

    DragOverable. New component first commit

---
+++ src/app/DragOverable/component.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { DragOverableProps } from './container';
+
+/** Materials */
+import Paper from '@material-ui/core/Paper';
+
+const DragOverableComponent: React.StatelessComponent<DragOverableProps> = props => {
+    const { onDragOver, onDrop, ...other } = props;
+
+    const _onDragOver = (e) => {
+        onDragOver ||
+        e.preventDefault();
+    };
+
+    return (
+        <Paper
+            onDragOver={(e) => _onDragOver(e)}
+            {...{ onDrop, other }}
+        >
+            {props.children}
+        </Paper>
+    );
+};
+
+export default DragOverableComponent;
