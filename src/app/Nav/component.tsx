cdc647777f213ba1668dc4b128e26dc8c07da090
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-26 20:54:41 +0100

    Nav. Rules included

--- src/app/Nav/component.tsx
+++ src/app/Nav/component.tsx
@@ -10,7 +10,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Face from '@material-ui/icons/Face';
 import Home from '@material-ui/icons/Home';
 
-import { MenuContainers } from '../../_common/';
+import { MenuContainers, NavRulesEnum } from '../../_common/';
 const { userMenu, mainMenu } = MenuContainers;
 
 const appBarColor = 'primary';
@@ -19,28 +19,33 @@ import * as submenus from './submenus';
 import { __SubMenuProps } from '../SubMenu/container';
 
 const NavComponent: React.StatelessComponent<NavProps> = props => {
-    const subMenus = [
+    const { notAnyLesson, notHome } = NavRulesEnum;
+
+    const subMenus: __SubMenuProps[] = [
         {
             menuItem: submenus.homeMenuItem,
-            icon: <Home />
+            icon: <Home />,
+            rules: [ notAnyLesson, notHome ]
         },
         {
             menuItems: submenus.userMenuItems,
             icon: <Face />,
             container: userMenu,
+            rules: [ notAnyLesson, notHome ]
         },
         {
             menuItems: submenus.mainMenuItems,
             icon: <MenuIcon />,
-            container: mainMenu
+            container: mainMenu,
+            rules: [ notAnyLesson, notHome ]
         }
     ] as __SubMenuProps[];
 
-  return (
-    <AppBar color={appBarColor}>
-      <AppMenu {...{ subMenus }} />
-    </AppBar>
-  );
+    return (
+        <AppBar color={appBarColor}>
+            <AppMenu {...{ subMenus }} />
+        </AppBar>
+    );
 };
 
 export default NavComponent;
--- src/app/Nav/submenus.ts
+++ src/app/Nav/submenus.ts
@@ -1,16 +1,17 @@
-import { AppRoutes } from '../../_common/';
+import { AppRoutes, SubMenuRulesEnum } from '../../_common/';
+import { SubMenuItemType } from '../SubMenu/container';
 
 const { home, lessons, about, login, newuser } = AppRoutes;
+const { onlyAuthorized, onlyUnauthorized, notCurrentLocation} = SubMenuRulesEnum;
 
-export const homeMenuItem = ['Home', home];
+export const homeMenuItem: SubMenuItemType = [ 'Home', home, [ notCurrentLocation,  ]];
 
-export const mainMenuItems = [
-    ['Lessons', lessons],
-    ['About', about]
+export const mainMenuItems: SubMenuItemType[] = [
+    [ 'Lessons', lessons, [ notCurrentLocation ] ],
+    [ 'About', about, [ notCurrentLocation ] ]
 ];
 
-export const userMenuItems = [
-    ['Login', login],
-    ['Newuser', newuser]
+export const userMenuItems: SubMenuItemType[] = [
+    [ 'Login', login, [ onlyUnauthorized, notCurrentLocation,  ] ],
+    [ 'Newuser', newuser, [ onlyUnauthorized, notCurrentLocation,  ] ]
 ];
-
