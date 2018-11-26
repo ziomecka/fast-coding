934dd8e57155a3fe9b0afef1f429b1c836347206
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-26 20:52:12 +0100

    App _common, SubMenuRulesEnum and NavRulesEnum added

--- src/app/_common/index.ts
+++ src/app/_common/index.ts
@@ -51,4 +51,17 @@ export enum AppRoutes {
     newuser = '/newuser',
     demo = '/lessons/lesson-demo',
     lessonsGet = '/lessons/get'
-}
+};
+
+export enum SubMenuRulesEnum {
+    'onlyAuthorized' = 'ONLY_AUTHORIZED',
+    'onlyUnauthorized' = 'ONLY_UNAUTHORIZED',
+    'notCurrentLocation' = 'NOT_CURRENT_LOCATION'
+};
+
+export enum NavRulesEnum {
+    'notHome' = 'NOT_HOME',
+    'notLesson' = 'NOT_LESSON',
+    'notDemoLesson' = 'NOT_DEMO_LESSON',
+    'notAnyLesson' = 'NOT_ANY_LESSON'
+};
