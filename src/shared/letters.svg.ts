df7603bc6fb8528c36851e82dfb03e0611a0bd55
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-27 19:48:39 +0100

    SVG Letters, object created

---
+++ src/shared/letters.svg.ts
@@ -0,0 +1,39 @@
+// @ts-ignore
+import a from '../svg/letters/a.svg';
+// @ts-ignore
+import d from '../svg/letters/d.svg';
+// @ts-ignore
+import e from '../svg/letters/e.svg';
+// @ts-ignore
+import g from '../svg/letters/g.svg';
+// @ts-ignore
+import k from '../svg/letters/k.svg';
+// @ts-ignore
+import l from '../svg/letters/l.svg';
+// @ts-ignore
+import o from '../svg/letters/o.svg';
+// @ts-ignore
+import p from '../svg/letters/p.svg';
+// @ts-ignore
+import b from '../svg/letters/b.svg';
+// @ts-ignore
+import c from '../svg/letters/c.svg';
+// @ts-ignore
+import i from '../svg/letters/i.svg';
+// @ts-ignore
+import s from '../svg/letters/s.svg';
+
+export default {
+    'a': a,
+    'd': d,
+    'e': e,
+    'g': g,
+    'k': k,
+    'l': l,
+    'o': o,
+    'p': p,
+    'b': b,
+    'c': c,
+    'i': i,
+    's': s,
+};
