b6928474c65688aee1511880977c8917f478deae
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-28 20:40:52 +0100

    LessonButtons. Autofocus added. Buttons focus managed Listen to tab and enter

--- src/components/Lesson/LessonButtons/_duck/operations.ts
+++ src/components/Lesson/LessonButtons/_duck/operations.ts
@@ -7,6 +7,7 @@ import { openDialog, closeDialog } from '../../../../app/Dialog/_duck/actions';
 import { onReset } from '../../_duck/operations';
 
 import history from '../../../../shared/history';
+import { manageButtonFocus as buttonFocus } from '../../../../shared/button.focus';
 
 const leave = (dispatch: Dispatch) => {
     dispatch(onReset());
@@ -16,16 +17,22 @@ const leave = (dispatch: Dispatch) => {
 
 const cancelLeave = (dispatch: Dispatch) => dispatch(closeDialog());
 
+const buttonsIds = [ 'dialogLeave', 'dialogCancel' ];
+const manageButtonFocus = buttonFocus(buttonsIds, 1);
+
+const onKeyDown = (e: KeyboardEvent): void => manageButtonFocus(e);
+
 export const onStartLeaving = (): any => (dispatch: Dispatch) => {
     dispatch(
         openDialog({
             buttons: [
-                [ 'leave', () => leave(dispatch) ],
-                [ 'cancel', () => cancelLeave(dispatch), {color: "secondary"} ]
+                [ 'leave', () => leave(dispatch), { id: buttonsIds[1] }],
+                [ 'cancel', () => cancelLeave(dispatch), { id: buttonsIds[0], color: 'secondary', autoFocus: `{true}` } ]
             ],
-            title: "Do you want to leave lesson?",
-            message: "Are you sure?",
-            onClose: () => cancelLeave(dispatch)
+            title: 'Do you want to leave lesson?',
+            message: 'Are you sure?',
+            onClose: () => cancelLeave(dispatch),
+            onKeyDown: (e: KeyboardEvent) => onKeyDown(e)
         }
     ));
 };
