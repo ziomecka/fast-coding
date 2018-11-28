2634151227a110244a41fda406020c151ef1dc34
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-28 20:52:00 +0100

    Dialog. Unnecessary code removed. Dispatch removed.

--- src/app/Dialog/container.tsx
+++ src/app/Dialog/container.tsx
@@ -1,17 +1,9 @@
-import * as React from 'react';
-import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
 import { default as Dialog } from './component';
 import { ApplicationState } from '../../store';
 
-import { closeDialog, openDialog } from './_duck/actions';
-
 import { DialogState } from './_duck/reducers';
-import { default as operations } from './_duck/';
-
-
-const { } = operations;
 
 import { ApplicationContainers, AppContainers } from '../../_common/';
 
@@ -22,18 +14,8 @@ const mapStateToProps = (state: ApplicationState): DialogState => ({
     ...state[app][dialog]
 });
 
-const mapDispatchToProps = (dispatch: Dispatch): DialogDispatch => ({
-    dialogOpen: () => dispatch(openDialog()),
-    dialogClose: () => dispatch(closeDialog())
-});
-
-const DialogContainer = connect(mapStateToProps, mapDispatchToProps)(Dialog);
+const DialogContainer = connect(mapStateToProps)(Dialog);
 
 export default DialogContainer;
 
-export interface DialogDispatch {
-    dialogOpen: () => void,
-    dialogClose: () => void
-};
-
-export interface AppDialogProps extends DialogDispatch, DialogState {};
+export interface AppDialogProps extends DialogState {};
