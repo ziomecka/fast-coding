0e389512e85cc5a02841f825fb38adf88b40b9a1
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-29 11:44:48 +0100

    Nav. Connected to store and languages menu added

--- src/app/Nav/component.tsx
+++ src/app/Nav/component.tsx
@@ -9,26 +9,44 @@ import AppBar from '@material-ui/core/AppBar';
 import MenuIcon from '@material-ui/icons/Menu';
 import Face from '@material-ui/icons/Face';
 import Home from '@material-ui/icons/Home';
+import Language from '@material-ui/icons/Language';
 
 /* Materials */
 import withStyles from '@material-ui/core/styles/withStyles';
 import style from './style';
 
 import { MenuContainers, NavRulesEnum } from '../../_common/';
-const { userMenu, mainMenu } = MenuContainers;
+const { userMenu, mainMenu, languagesMenu } = MenuContainers;
 
 const appBarColor = 'primary';
 
 import * as submenus from './submenus';
 import { __SubMenuProps } from '../SubMenu/container';
+import { withLocalize } from 'react-localize-redux';
 
 const NavComponent: React.StatelessComponent<NavProps> = props => {
     const { notAnyLesson, notHome, onlyAbout, notAbout } = NavRulesEnum;
     const { classes } = props;
+    const { languages, setActiveLanguage } = props;
 
     const { lessons } = classes;
 
     const subMenus: __SubMenuProps[] = [
+        {
+            menuItems: Object.keys(languages)
+                .reduce((acc, cv) => {
+                    const { name, code } = languages[cv];
+                    acc.push({
+                        title: name,
+                        onClick: () => {
+                            setActiveLanguage(code)
+                        } });
+                    return acc;
+                }, []),
+            icon: <Language />,
+            container: languagesMenu,
+            rules: []
+        },
         {
             menuItem: submenus.lessonsMenuItem,
             icon: <span className={lessons}> Lessons </span>,
@@ -60,4 +78,4 @@ const NavComponent: React.StatelessComponent<NavProps> = props => {
     );
 };
 
-export default withStyles(style)(NavComponent);
+export default withStyles(style)(withLocalize(NavComponent));
--- src/app/Nav/container.tsx
+++ src/app/Nav/container.tsx
@@ -1,5 +1,13 @@
+import { connect } from 'react-redux';
 import { default as NavComponent } from './component';
 import { WithStyles } from '@material-ui/core';
-export default NavComponent;
 
-export interface NavProps extends WithStyles {};
+const mapStateToProps = state => ({
+    ...state.localize
+});
+
+export default connect(mapStateToProps)(NavComponent);
+
+import { LocalizeContextProps } from 'react-localize-redux';
+
+export interface NavProps extends WithStyles, LocalizeContextProps {};
