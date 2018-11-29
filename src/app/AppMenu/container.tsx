1efed6d5c5fc7950074772e5218952e38b86ee58
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-29 17:27:35 +0100

    Nav, AppMenu. withRouter added. Without it the SubMenu does not see the correct pathname

--- src/app/AppMenu/container.tsx
+++ src/app/AppMenu/container.tsx
@@ -1,9 +1,13 @@
 import { default as AppMenuComponent } from './component';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 import { __SubMenuProps } from '../SubMenu/container';
 import { WithStyles } from '@material-ui/core';
-export default AppMenuComponent;
 
-export interface AppMenuProps extends WithStyles {
+export default withRouter(AppMenuComponent);
+
+export interface AppMenuProps extends
+    RouteComponentProps<{}>,
+    WithStyles {
     subMenus: __SubMenuProps[];
 };
--- src/app/Nav/container.tsx
+++ src/app/Nav/container.tsx
@@ -1,3 +1,4 @@
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { default as NavComponent } from './component';
 import { WithStyles } from '@material-ui/core';
@@ -6,8 +7,8 @@ const mapStateToProps = state => ({
     ...state.localize
 });
 
-export default connect(mapStateToProps)(NavComponent);
+export default withRouter(connect(mapStateToProps)(NavComponent));
 
 import { LocalizeContextProps } from 'react-localize-redux';
 
-export interface NavProps extends WithStyles, LocalizeContextProps {};
+export interface NavProps extends WithStyles, LocalizeContextProps, RouteComponentProps<{}> {};
