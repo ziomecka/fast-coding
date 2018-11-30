dd76f57fe7527aa1d09fad43c6dd907dd6e074fc
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-30 17:32:42 +0100

    Welcome. Moved from home to Nav. Better location, also for styling purposes.

--- src/app/Nav/component.tsx
+++ src/app/Nav/component.tsx
@@ -3,6 +3,13 @@ import * as React from 'react';
 import { NavProps } from './container';
 
 import AppMenu from '../../app/AppMenu';
+import Welcome from '../../app/Welcome/';
+import {
+    HOME_HEADING_ANIMATED,
+    HOME_NOTIFICATION,
+    HOME_WELCOME_TIMEOUT
+} from '../../constants';
+import { getTranslations, getActiveLanguage, getLanguages } from 'react-localize-redux';
 
 /** Materials */
 import AppBar from '@material-ui/core/AppBar';
@@ -64,6 +71,18 @@ const NavComponent: React.StatelessComponent<NavProps> = props => {
 
     return (
         <AppBar color={appBarColor}>
+            <Welcome
+                heading={
+                    getTranslations(props.localize).welcomeHeading[
+                        getLanguages(props.localize)
+                        .findIndex(lang => (
+                            lang.code === getActiveLanguage(props.localize).code)
+                        )
+                    ]
+                }
+                animated={HOME_HEADING_ANIMATED}
+                timeout={HOME_WELCOME_TIMEOUT}
+            />
             <AppMenu {...{ subMenus }} />
         </AppBar>
     );
