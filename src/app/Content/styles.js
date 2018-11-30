5893e16afeee61f7f228ff5edbef01fbed0c9e57
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-11-30 15:17:49 +0100

    Content. Leading spaces corrected.

--- src/app/Content/styles.js
+++ src/app/Content/styles.js
@@ -4,29 +4,29 @@ const styles = theme => {
     const {  palette: { background: { default: defaultBackground }}} = theme;
 
     return {
-    contentBox: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        position: 'relative',
-        width: '100%',
-        maxWidth: '100%', // do not allow to increase size if draggable moved outside
-        paddingTop: CONTENT_PADDING_TOP,
-        overflow: 'hidden',
-        backgroundColor: defaultBackground
-    },
-    contentBoxHome: {
-        height: '0',
-        padding: '0',
-        overflow: 'hidden'
-    },
-    contentBoxOther: {
-        boxSizing: 'border-box',
-        display: 'flex',
-        minHeight: 'calc(100vh)'
-    }
-};
+        contentBox: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            position: 'relative',
+            width: '100%',
+            maxWidth: '100%', // do not allow to increase size if draggable moved outside
+            paddingTop: CONTENT_PADDING_TOP,
+            overflow: 'hidden',
+            backgroundColor: defaultBackground
+        },
+        contentBoxHome: {
+            height: '0',
+            padding: '0',
+            overflow: 'hidden'
+        },
+        contentBoxOther: {
+            boxSizing: 'border-box',
+            display: 'flex',
+            minHeight: 'calc(100vh)'
+        }
+    };
 };
 
 export default styles;
