3a38788716c0a93ed6df7b4df60770eeae20c7c4
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-01 17:08:18 +0100

    Lessons. onOpenLesson send to lesson lessonTextin active language

--- src/components/Lessons/_duck/operations.ts
+++ src/components/Lessons/_duck/operations.ts
@@ -4,6 +4,11 @@ import { turnOnTextGenerator } from '../../TextGenerator/_duck/actions';
 import { updateLesson } from '../../Lesson/_duck/actions';
 
 import { LessonData } from '../../Lesson/_duck/reducers';
+import { openLesson } from '../../Lesson/_duck/actions';
+
+import { ApplicationState } from '../../../_reducers/';
+
+import { getActiveLanguage } from 'react-localize-redux';
 
 export const onOpenRandomLesson = (lesson: LessonData): any => (
     async (dispatch: Dispatch ) => {
@@ -14,3 +19,13 @@ export const onOpenRandomLesson = (lesson: LessonData): any => (
         return response;
     }
 );
+
+export const onOpenLesson = (lessonData: LessonData): any => (dispatch: Dispatch, getState: () => ApplicationState) => {
+    const { code } = getActiveLanguage(getState().localize);
+
+    lessonData.lessonText = (lessonData.translatedTexts && lessonData.translatedTexts[code]) ||
+        lessonData.text ||
+        '';
+
+    return dispatch(openLesson(lessonData));
+};
--- src/components/Lessons/container.tsx
+++ src/components/Lessons/container.tsx
@@ -7,23 +7,25 @@ import { WithStyles } from '@material-ui/core/styles';
 import { default as Lessons } from './component';
 
 import { LessonData } from '../Lesson/_duck/reducers';
-import { openLesson } from '../Lesson/_duck/actions';
 
-import { onOpenRandomLesson } from './_duck/operations';
+import { onOpenRandomLesson, onOpenLesson } from './_duck/operations';
 import { ApplicationContainers, ComponentsContainers } from '../../_common/';
 import ApplicationState from '../../_reducers/';
 
 import { LessonsLoaderState } from '../LessonsLoader/_duck/reducers';
 
+import { LocalizeState} from 'react-localize-redux';
+
 const { components } = ApplicationContainers;
 const { lessonsLoader } = ComponentsContainers;
 
-const mapStateToProps = (state: ApplicationState): LessonsLoaderState => ({
-    ...state[components][lessonsLoader]
+const mapStateToProps = (state: ApplicationState): MapStateToProps => ({
+    ...state[components][lessonsLoader],
+    localize: state.localize
 });
 
 const mapDispatchToProps = (dispatch: Dispatch): LessonsDispatch => ({
-    handleOpenLesson: (lessonData) => dispatch(openLesson(lessonData)),
+    handleOpenLesson: (lessonData) => dispatch(onOpenLesson(lessonData)),
     handleOpenRandomLesson: (lessonData) => dispatch(onOpenRandomLesson(lessonData))
 });
 
@@ -31,10 +33,18 @@ const LessonsContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)
 
 export default LessonsContainer;
 
+interface MapStateToProps extends LessonsLoaderState {
+    localize: LocalizeState
+}
+
 export interface LessonsDispatch {
     handleOpenLesson: (lessonData: LessonData) => void;
     handleOpenRandomLesson: (lessonData: LessonData) => void;
 };
 
-export interface LessonsProps extends LessonsDispatch, LessonsLoaderState, RouteComponentProps<{}>, WithStyles {
-};
+export interface LessonsProps extends
+    LessonsDispatch,
+    LessonsLoaderState,
+    RouteComponentProps<{}>,
+    WithStyles,
+    MapStateToProps {};
