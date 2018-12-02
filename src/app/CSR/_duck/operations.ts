2fd856014705ad388babb0efeeec35b6a597aa0e
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-02 15:43:14 +0100

    CSR. Operations: dispatch in action object with data, in whih key is the state. If data found in local storage: dispatch changeLoadingState to false

--- src/app/CSR/_duck/operations.ts
+++ src/app/CSR/_duck/operations.ts
@@ -14,20 +14,36 @@ import { LocalStorageItemTypes } from '../../_common/index';
 export const onLoadData =
 (url: string, applicationContainer: ApplicationContainers, container: ComponentsContainers | AppContainers, lsItem: LocalStorageItemTypes, stateName: string): any => (
     async (dispatch: Dispatch): Promise<any> => {
-        dispatch(updateData(localStorageGetItem(lsItem), applicationContainer, container));
 
+        /** Loading => true */
         dispatch(changeLoadingState(true, applicationContainer, container));
+
+        let localStorage = localStorageGetItem(lsItem);
+
+        /** lsItem.toLowerCase() => string under which data will be saved in new state
+         *  see reducer
+        */
+        dispatch(updateData({ [`${lsItem.toLowerCase()}`]: localStorage }, applicationContainer, container));
+
+        /** If data was in local storage
+         *  Loading => false
+        */
+        if(localStorage) {
+            dispatch(changeLoadingState(false, applicationContainer, container, lsItem));
+            localStorage = null; // GC
+        }
 
         try {
             let data = await getData(url);
             dispatch(updateData({ [stateName]: data }, applicationContainer, container));
             localStorageSetItem(lsItem, data);
-            // console.log('in LS')
-            // console.log(localStorageGetItem(lsItem))
-            // console.log('end')
         }
         catch (err) {
-            dispatch(reportError());
+            dispatch(reportError(
+                err,
+                applicationContainer,
+                container
+            ));
         }
         finally {
             dispatch(changeLoadingState(false, applicationContainer, container));
