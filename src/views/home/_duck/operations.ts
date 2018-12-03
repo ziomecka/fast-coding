e047f0ad37ee24f8b4d6dff13f8f13c0bd55a7f5
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-03 14:45:35 +0100

    Home view. Listener to any key on home view removed. The listner was pushing to lessons. Was user unfriendly

--- src/views/home/_duck/operations.ts
+++ src/views/home/_duck/operations.ts
@@ -1,8 +1,5 @@
 import { manageButtonFocus as buttonFocus } from '../../../shared/button.focus';
 import keydownListeners from '../../../shared/keydown.listener';
-import history from '../../../shared/history';
-import { AppRoutes } from '../../../_common/';
-const { lessons } = AppRoutes;
 
 export const buttonsIds = [ 'homeSeeLessons', 'homeStartTyping' ];
 
@@ -10,14 +7,6 @@ const manageButtonFocus = buttonFocus(buttonsIds, 1);
 const manageKeydownListeners = keydownListeners();
 
 const manageFocus = (e: KeyboardEvent): void => manageButtonFocus(e);
-
-/** If code other than tab or return then move to lessons */
-const moveToLessons = (e: KeyboardEvent): void => {
-    const { keyCode } = e;
-    if (keyCode !== 9 && keyCode !== 13) {
-        history.push(lessons);
-    }
-};
 
 export const onAddKeyDownListener = () => {
     manageKeydownListeners.addKeyDownListener(manageFocus);
