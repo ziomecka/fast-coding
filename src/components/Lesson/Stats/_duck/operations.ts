a0c4c7b8b58af829e43e1b38e643f399c6473512
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-03 12:10:21 +0100

    Stats. Pause, Unpause actions. onStartTimer, onStopTimer, onPauseTimer, onUnpauseTimer operations added

--- src/components/Lesson/Stats/_duck/operations.ts
+++ src/components/Lesson/Stats/_duck/operations.ts
@@ -1,1 +1,21 @@
+import { Dispatch } from 'redux';
+import { startTimer, stopTimer, pauseStats, unpauseStats } from './actions';
+
+
+export const onStartTimer = (): any => (dispatch: Dispatch) => {
+    dispatch(startTimer());
+};
+
+export const onStopTimer = (): any => (dispatch: Dispatch) => {
+    return dispatch(stopTimer());
+};
+
+export const onPauseTimer = (): any => (dispatch: Dispatch) => {
+    return dispatch(pauseStats());
+};
+
+export const onUnpauseTimer = (): any => (dispatch: Dispatch) => {
+    return dispatch(unpauseStats());
+};
+
 export default {};
