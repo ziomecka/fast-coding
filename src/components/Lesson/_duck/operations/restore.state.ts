6f2cc4295de6f07848ac624dd31ece5eceb7c73e
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-10 19:10:02 +0100

    Lesson. restore.state typing, return promise, default: do not clear localStorage when restoring state

--- src/components/Lesson/_duck/operations/restore.state.ts
+++ src/components/Lesson/_duck/operations/restore.state.ts
@@ -18,30 +18,46 @@ import { RestoreStateAction as ComparatorRestoreStateAction } from '../../Compar
 
 import { localStorageSetItem, localStorageGetItem, localStorageRemoveItem } from '../../../../app/LocalStorage/_duck/operations';
 
-export const onKeepState = (localStorageItem: LocalStorageItemTypes, container: ComponentsContainers): any => (dispatch: Dispatch, getState: ThunkGetStateType) => {
-    localStorageSetItem(localStorageItem, getState()[components][container]);
-};
+export const onKeepState = (localStorageItem: LocalStorageItemTypes, container: ComponentsContainers): any => (
+    (dispatch: Dispatch, getState: ThunkGetStateType): boolean => (
+        localStorageSetItem(localStorageItem, getState()[components][container])
+    )
+);
 
-export const onRemoveState = (localStorageItem: LocalStorageItemTypes): any => (dispatch: Dispatch, getState: ThunkGetStateType) => {
-    localStorageRemoveItem(localStorageItem);
-};
+export const onRemoveState = (localStorageItem: LocalStorageItemTypes): any => (
+    (dispatch: Dispatch, getState: ThunkGetStateType): boolean => (
+        localStorageRemoveItem(localStorageItem)
+));
 
 export const onRestoreState = (
     localStorageItem: LocalStorageItemTypes,
-    action: (data) => LessonRestoreStateAction | StatsRestoreStateAction | ComparatorRestoreStateAction
+    action: (data) => LessonRestoreStateAction | StatsRestoreStateAction | ComparatorRestoreStateAction,
+    clearState: boolean = false
     ): any => (
-    async (dispatch: Dispatch) => {
-        let data = localStorageGetItem(localStorageItem);
+        async (dispatch: Dispatch): Promise<boolean> => {
+            let data = localStorageGetItem(localStorageItem);
 
-        if (data) {
-            let answer = await dispatch(action(data as LessonState | StatsState | ComparatorState));
-            if (answer) {
-                data = null;
-                answer = null;
+            if (data) {
+                let answer = await dispatch(action(data as LessonState | StatsState | ComparatorState));
+
+                /**
+                 * Comparator and stats require clearing state
+                 * But it is done when new lesson is opened. Not here.
+                 * */
+                if (clearState) {
+                    dispatch(onRemoveState(localStorageItem));
+                }
+
+                if (answer) {
+                    data = null;
+                    answer = null;
+                    return true;
+                }
             }
+
+            return Promise.resolve(false);
         }
-    }
-);
+    );
 
 export default {
     onKeepState,
