82c548e86ca25a36e2e4a2260351f4167696c4fa
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-14 00:41:31 +0100

    Deploy

--- deploy/constants.js
+++ deploy/constants.js
@@ -1,3 +1,7 @@
 const PORT = 3000;
 
-module.exports = { PORT };
+/** Cookies */
+const HASH_COOKIE_MAX_AGE = 900000;
+const HASH_COOKIE_NAME = 'fc-hash';
+
+module.exports = { PORT, HASH_COOKIE_MAX_AGE, HASH_COOKIE_NAME };
