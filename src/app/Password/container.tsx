44e4d37115fbbf6f08633c7e40ef660970e3dcc6
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-13 10:56:42 +0100

    Password. Localize added to container.

--- src/app/Password/container.tsx
+++ src/app/Password/container.tsx
@@ -11,10 +11,13 @@ import { AppContainers, ApplicationContainers, PasswordTypes } from '../../_comm
 
 import { StandardTextFieldProps } from '@material-ui/core/TextField';
 
+import { LocalizeState } from 'react-localize-redux';
+
 const { app } = ApplicationContainers;
 
-const mapStateToProps = (state: ApplicationState): AppState => ({
-    ...state[app]
+const mapStateToProps = (state: ApplicationState): MapStateToPropsI => ({
+    ...state[app],
+    localize: state.localize
 });
 
 const mapDispatchToProps = (dispatch: Dispatch): PasswordDispatch => ({
@@ -25,6 +28,10 @@ const PasswordContainer = connect(mapStateToProps, mapDispatchToProps)(Password)
 
 export default PasswordContainer;
 
+interface MapStateToPropsI extends AppState {
+    localize: LocalizeState
+};
+
 export interface PasswordDispatch {
     setPassword: (
         password: string,
@@ -33,7 +40,7 @@ export interface PasswordDispatch {
     ) => void;
 };
 
-export interface PasswordProps extends PasswordDispatch, AppState, StandardTextFieldProps {
+export interface PasswordProps extends PasswordDispatch, MapStateToPropsI, StandardTextFieldProps {
     container: AppContainers;
     passwordType: PasswordTypes;
 };
