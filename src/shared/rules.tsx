4ee472180f7aa7474112daa3a8f7a3979b3ca2bd
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-13 22:17:08 +0100

    Shared rules. New rules added. rulesRegexp added, interfaces added, applyRules changed: one argument.

--- src/shared/rules.tsx
+++ src/shared/rules.tsx
@@ -1,11 +1,24 @@
 import { invalidError } from './_types/';
 export { invalidError };
 
-const { NOT_EMPTY, NO_SPACES } = invalidError;
+const { NOT_EMPTY, NO_SPACES, NOT_LONG, NOT_EMAIL, NO_DIGIT, NO_SPECIALS, NO_MATCH } = invalidError;
 
-const rules = {
-    [NO_SPACES]: value => !(/.*[\s].*/.test(value)),
-    [NOT_EMPTY]: value => value && value.length
+export const rulesRegexp: RulesRegexpI = {
+    [NO_SPACES]: () => /.*[\s].*/,
+    [NOT_LONG]: (min, max) => RegExp(`^.{${min},${max}}$`),
+    [NOT_EMAIL]: () => /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    [NO_DIGIT]: () => /^.*[0-9]{1,}.*$/,
+    [NO_SPECIALS]: () => /.*[^a-zA-Z0-9]+.*/
+};
+
+const rules: RulesI = {
+    [NO_SPACES]: options => !( rulesRegexp[NO_SPACES]().test(options.value) ),
+    [NOT_EMPTY]: options => options.value && !!options.value.length,
+    [NOT_LONG]: options => ( rulesRegexp[NOT_LONG](options.min, options.max).test(options.value) ),
+    [NOT_EMAIL]: options => ( rulesRegexp[NOT_EMAIL]().test(options.value) ),
+    [NO_DIGIT]: options => ( rulesRegexp[NO_DIGIT]().test(options.value) ),
+    [NO_SPECIALS]: options => ( rulesRegexp[NO_SPECIALS]().test(options.value) ),
+    [NO_MATCH]: options => options.value === options.value2
 };
 
 import { LocalizeState } from 'react-localize-redux';
@@ -15,13 +28,40 @@ export const helperTexts = (rule, value: string, localize: LocalizeState): strin
     getTranslation(localize, `${rule}_${value.toUpperCase()}`)
 );
 
-export const applyRules = (value: string, _rules: invalidError[] = [], localize: LocalizeState): string | undefined => {
-    for (const rule in _rules) {
-        const ruleName = _rules[rule];
+export const applyRules = (options: ApplyRulesType): string | undefined => {
+
+    for (const rule in options) {
+        const { value, min = 8, max = 15, value2, opposite } = options[rule][1];
+
+        const ruleName = options[rule][0];
+
         const ruleEnum = invalidError[ruleName];
-        if(!rules[ruleEnum](value)) {
+
+        const result = !rules[ruleEnum]({ value, min, max, value2 });
+
+        if ( ( result && !opposite ) || ( !result && opposite ) ) {
             return ruleEnum;
         }
     }
+
     return undefined;
+};
+
+/** Interfaces */
+export type ApplyRulesType = Array<[ invalidError, RulesDataI ]>;
+
+interface RulesRegexpI {
+    [key: string]: (min?: number, max?: number) => RegExp
+};
+
+export interface RulesDataI {
+    value: string;
+    min?: number;
+    max?: number;
+    value2?: string;
+    opposite?: boolean;
+};
+
+interface RulesI {
+    [key: string]: (options: RulesDataI) => boolean
 };
