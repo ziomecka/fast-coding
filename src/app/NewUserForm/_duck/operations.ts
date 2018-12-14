be5f68bdced18b99f4f4df4df7db64c2721bf079
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-14 18:14:45 +0100

    NewUserForm LoginForm Localize. onLog success and setNewUser succes. redirect to lessons, set authorized state and open notification

--- src/app/NewUserForm/_duck/operations.ts
+++ src/app/NewUserForm/_duck/operations.ts
@@ -1,18 +1,22 @@
 import { Dispatch, Action } from 'redux';
 
 import { post as postData } from '../../api/';
-import { AppRoutes } from '../../../_common/';
+import { AppRoutes, ThunkGetStateType } from '../../../_common/';
+
 import { NewUserFormResponseEnum } from './types';
 
 import { setFormHelperText } from '../../FormHelperText/_duck/actions';
+import { authorizeUser } from  '../../User/_duck/actions';
+import history from '../../../shared/history';
+
+import { onOpenNotification } from '../../Notification/_duck/operations';
+import getTranslation from '../../../shared/get.translation';
 
 const { SUCCESS } = NewUserFormResponseEnum;
-const { newUserSet } = AppRoutes;
-
-const sendNewUserFormSuccess = (): any => async ( dispatch: Dispatch ): Promise<any> => {};
+const { newUserSet, lessons } = AppRoutes;
 
 export const onSendNewUserForm = (login: string, password: string, email: string ): any => (
-    async ( dispatch: Dispatch ): Promise<Action> => {
+    async ( dispatch: Dispatch, getState: ThunkGetStateType ): Promise<Action> => {
         /** removes formInvalid message */
         dispatch(setFormHelperText('formBeingSent'));
 
@@ -21,12 +25,12 @@ export const onSendNewUserForm = (login: string, password: string, email: string
         const { result } = JSON.parse(response || null);
 
         if (result === SUCCESS) {
-            dispatch(setFormHelperText(null)); // TODO should not be needed if redirected
-            return dispatch(sendNewUserFormSuccess());
+            dispatch(authorizeUser(login));
+            history.push(lessons);
+            return dispatch(onOpenNotification(getTranslation(getState().localize, 'notificationAuthorized')));
         }
-        else {
-            return dispatch(setFormHelperText(NewUserFormResponseEnum[result]));
-        }
+
+        return dispatch(setFormHelperText(NewUserFormResponseEnum[result]));
     }
 );
 
