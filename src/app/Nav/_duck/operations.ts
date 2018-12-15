eaad23eeb3a47ea5e6b17aacd1c03d2a138dc1b7
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-15 16:30:23 +0100

    Notification. getTraslation in onOpenNotification

--- src/app/Nav/_duck/operations.ts
+++ src/app/Nav/_duck/operations.ts
@@ -3,14 +3,13 @@ import { Dispatch, Action } from 'redux';
 import { unauthorizeUser } from '../../User/_duck/actions';
 import { onOpenNotification } from '../../Notification/_duck/operations';
 
-import getTranslation from '../../../shared/get.translation';
-import { ThunkGetStateType, AppRoutes } from '../../../_common/';
+import { AppRoutes } from '../../../_common/';
 import history from '../../../shared/history';
 
 const { lessons } = AppRoutes;
 
 export const onLogOut = (): any => (
-    async (dispatch: Dispatch, getState: ThunkGetStateType): Promise<Action> => {
+    async (dispatch: Dispatch): Promise<Action> => {
         let response = await dispatch(unauthorizeUser());
 
         // TODO if not try catch?
@@ -18,7 +17,7 @@ export const onLogOut = (): any => (
         if (response) {
             response = null;
             history.push(lessons);
-            return dispatch(onOpenNotification(getTranslation(getState().localize, 'notificationSignOutSuccess')));
+            return dispatch(onOpenNotification('notificationSignOutSuccess'));
         }
 });
 
--- src/app/Notification/_duck/operations.ts
+++ src/app/Notification/_duck/operations.ts
@@ -2,15 +2,18 @@ import { Dispatch } from 'redux';
 
 import { openNotification, closeNotification, setTimeNotification } from './actions';
 import { NOTIFICATION_DURATION } from '../../../constants';
+import getTranslation from '../../../shared/get.translation';
+import { ThunkGetStateType } from '../../../_common';
 
 let timeout;
 
 export const onOpenNotification = (text: string, time: number = NOTIFICATION_DURATION): any => (
-    async (dispatch: Dispatch): Promise<any> => {
+    async (dispatch: Dispatch, getState: ThunkGetStateType ): Promise<any> => {
         let answer = dispatch(setTimeNotification(time));
 
         if (answer) {
-            dispatch(openNotification(text));
+            dispatch(openNotification(getTranslation(getState().localize, text)));
+
             timeout = setTimeout(() => {
                 dispatch(closeNotification());
                 clearTimeout(timeout);
