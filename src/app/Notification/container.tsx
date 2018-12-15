a3fa58e0580df5aa20e487de4a4150ccd8c0b673
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-15 19:02:26 +0100

    Notification. resetNotification action removed. Notification reset on open. setNotification before opening

--- src/app/Notification/container.tsx
+++ src/app/Notification/container.tsx
@@ -1,10 +1,7 @@
-import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
 import { default as Notification } from './component';
 import { ApplicationState } from '../../store';
-
-import { resetNotification } from './_duck/actions';
 
 import { NotificationState } from './_duck/reducers';
 
@@ -19,18 +16,10 @@ const mapStateToProps = (state: ApplicationState): NotificationState => ({
     ...state[app][notification]
 });
 
-const mapDispatchToProps = (dispatch: Dispatch): NotificationDispatch => ({
-    notificationReset: () => dispatch(resetNotification())
-});
-
-const NotificationContainer = connect(mapStateToProps, mapDispatchToProps)(Notification);
+const NotificationContainer = connect(mapStateToProps)(Notification);
 
 export default NotificationContainer;
 
-export interface NotificationDispatch {
-    notificationReset: () => void,
-};
-
-export interface AppNotificationProps extends NotificationDispatch,
+export interface AppNotificationProps extends
     NotificationState,
     WithStyles {};
