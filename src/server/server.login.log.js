66d05fc86cf481d282c9fdf481d3e351d2842e3b
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-15 11:52:07 +0100

    Revert "PasswordManager promisified. servver.login and server.newuser do not use callbacks and return jsons" because contained also passwordChange
    This reverts commit cb5c18f4ec8474ae15f57ffb50cf09958d15bd0b.

--- src/server/server.login.log.js
+++ src/server/server.login.log.js
@@ -1,14 +1,18 @@
 const passwordManager = require('./PasswordManager/');
 
 module.exports = async (req, res) => {
-    const { login, password } = req.body;
-
-    if ( !login || ! password ) {
-        res.json({ error: 'Login and password cannot be empty' });
-    }
-
     try {
-        res.json({ result: await passwordManager.verifyPassword({ login, password }) });
+        const { login, password } = req.body;
+
+        if ( !login || ! password ) {
+            res.json({ error: 'Login and password cannot be empty' });
+        }
+
+        passwordManager.verifyPassword({ login, password, callback: response => {
+            /** If password set succesfully set cookie */
+            res.json(JSON.stringify(response));
+        } });
+
     } catch (err) {
         res.json({ error: err.message || err.toString() });
     }
