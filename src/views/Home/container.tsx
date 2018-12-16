ce36627a97bda30a86acfd0f4cb7e8f16a7de95d
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-16 13:55:35 +0100

    Welcome and Home. Keyboard listener for manageButtonFocus moved from Home to Welcome

--- src/views/Home/container.tsx
+++ src/views/Home/container.tsx
@@ -5,24 +5,18 @@ import { default as HomeViewComponent } from './component';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 import { mapDispatchToProps as notificationMapDiaptchToProps, NotificationDispatch } from '../../shared/notification';
-import { onAddKeyDownListener, onRemoveKeyDownListener} from './_duck/operations';
 
 const mapStateToProps = () => ({});
 
 const mapDispatchToProps = (dispatch: Dispatch): HomeViewDispatch => ({
-    ...notificationMapDiaptchToProps(dispatch),
-    addKeyDownListener: () => onAddKeyDownListener(),
-    removeKeyDownListener: () => onRemoveKeyDownListener()
+    ...notificationMapDiaptchToProps(dispatch)
 });
 
 const HomeViewContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)(HomeViewComponent));
 
 export default HomeViewContainer;
 
-export interface HomeViewDispatch extends NotificationDispatch {
-    addKeyDownListener: () => void;
-    removeKeyDownListener: () => void;
-};
+export interface HomeViewDispatch extends NotificationDispatch {};
 
 export interface HomeViewProps extends
     HomeViewDispatch,
