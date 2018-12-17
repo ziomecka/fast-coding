cb91458d6ededde73472eb16cbbe58644c238ab2
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-17 01:17:36 +0100

    AppRouter. Moved from views index to seperate component. Connected to state

---
+++ src/views/AppRouter/component.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { Route, Router, Switch } from 'react-router-dom';
+import history from '../../shared/history';
+
+import HomeView from '../Home/';
+import LessonsView from '../Lessons/';
+import LessonView from '../Lesson/';
+import LoginView from '../Login/';
+import NewUserView from '../Newuser/';
+import ChangePasswordView from '../ChangePassword/';
+import RouteAuth from '../RouteAuth/';
+
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { theme } from '../../theme/';
+
+import { AppRoutes } from '../../_common/';
+
+import { LocalizeProvider } from 'react-localize-redux';
+import store from '../../store';
+
+const Root: React.StatelessComponent<{}> = () => {
+    const { lessons, login, newuser, changePassword } = AppRoutes;
+
+    return (
+        <MuiThemeProvider {...{ theme }}>
+            <LocalizeProvider {...{ store }} >
+                <Router {...{ history }}>
+                    <Route path="/">
+                    <HomeView>
+                            <Switch>
+                                <Route exact path={`${lessons}`} component={LessonsView} />
+                                <Route path={`${lessons}/:id`} component={LessonView} />
+                                <Route path={`${login}`} component={LoginView} />
+                                <Route path={`${newuser}`} component={NewUserView} />
+                                <RouteAuth path={`${ changePassword }`} component={ ChangePasswordView } />
+                            </Switch>
+                    </HomeView>
+                    </Route>
+                </Router>
+            </LocalizeProvider>
+        </MuiThemeProvider>
+    );
+};
+
+export default Root;
