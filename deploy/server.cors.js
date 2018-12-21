25c19ba5a10b08d9bb3b1bfad18c41ff60509085
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-21 17:01:50 +0100

    Deploy

--- deploy/server.cors.js
+++ deploy/server.cors.js
@@ -3,13 +3,10 @@ require('dotenv').config();
 const cors = require('cors');
 const CORS = require('./constants').CORS;
 
-const { localhost, production, exposedHeaders } = CORS;
+const { localhost, exposedHeaders } = CORS;
 
-<<<<<<< HEAD
-module.exports = (prod) => {
-    const whitelist = !prod
-        ? [ localhost ]
-        : [ production ];
+module.exports = () => {
+    const whitelist = [ localhost ];
 
     const corsOptions = {
         exposedHeaders,
@@ -25,22 +22,3 @@ module.exports = (prod) => {
 
     return cors(corsOptions);
 };
-=======
-const whitelist = !PROD_ENV
-    ? [ localhost ]
-    : [ production ];
-
-const corsOptions = {
-    exposedHeaders,
-    credentials: true,
-    origin: (origin, callback) => {
-        if ( !origin || whitelist.indexOf(origin) !== -1 ) {
-            callback(null, true);
-        } else {
-            callback(console.warn('Not allowed by CORS'), false);
-        }
-    }
-};
-
-module.exports = () => cors(corsOptions);
->>>>>>> develop
