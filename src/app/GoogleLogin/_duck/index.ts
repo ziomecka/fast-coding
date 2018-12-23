4846ca4f4ddfc7fae04975daff1601ca1c4de116
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-23 16:56:22 +0100

    Revert "GoogleLogin First commit"
    This reverts commit 47569e79b711eb8937dca318861998f3a31700c4.

--- src/app/GoogleLogin/_duck/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as GoogleLoginOperations } from './operations';
--- src/app/GoogleLogin/_duck/types.ts
+++
@@ -1,3 +0,0 @@
-export enum GoogleLoginActionsEnum {
-    APP_GOOGLE_LOGIN_FIREBASE_AUTHORIZE = '@@app_googleLogin/GET_GOOGLE_URL'
-};
