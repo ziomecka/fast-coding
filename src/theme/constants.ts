c73b311a961d0b14d79cf90f5a61f6dc777d9018
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-27 20:21:19 +0100

    Theme constants comments added

--- src/theme/constants.ts
+++ src/theme/constants.ts
@@ -1,12 +1,6 @@
 export * from '@app/Nav/constants.styles';
 export * from '@components/Lesson/constants.styles';
 
-export default {
-    navHeight: '60px',
-    navTop: 0,
-    navLeft: 0
-};
-
 /** PADDING */
 export const PAPER_PADDING_MD = '1rem';
 export const PAPER_PADDING_LG = '3rem';
@@ -14,12 +8,16 @@ export const PAPER_PADDING_LG = '3rem';
 export const PAPER_PADDING_MAX_MD = '1.5rem';
 export const PAPER_PADDING_MAX_LG = '6rem';
 
+/** ELEVATION */
 export const ELEVATION = 2;
 
+/** COLORS */
 export const COLOR_SUCCESS = '#c5ebc3';
 export const COLOR_CORRECTED = '#fcefe7';
 export const COLOR_HINTS = '#90ab8e';
 
+/** BORDER */
 export const BORDER_THIN = '.5px';
 
+/** BREAKPOINTS */
 export const MEDIA_DESKTOP = 1280;
