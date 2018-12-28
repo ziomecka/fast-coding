7c97e8da6cc228c2ccf0f9552079aa434ebb49a2
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-28 20:23:15 +0100

    LocalStorage localStorageGetItem try catch added

--- src/app/LocalStorage/_duck/operations.ts
+++ src/app/LocalStorage/_duck/operations.ts
@@ -1,9 +1,13 @@
 import { LocalStorageDataTypes } from './types';
 import { LocalStorageItemEnum } from '@appTypes';
 
-export const localStorageGetItem = (name: LocalStorageItemEnum): any => (
-    JSON.parse( localStorage.getItem(name) || null )
-);
+export const localStorageGetItem = (name: LocalStorageItemEnum): any => {
+    try {
+        return JSON.parse( localStorage.getItem(name) );
+    } catch (err) {
+        return null;
+    }
+};
 
 export const localStorageSetItem = (name: LocalStorageItemEnum, data: LocalStorageDataTypes): boolean => {
     localStorage.setItem(name, JSON.stringify(data));
