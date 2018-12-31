2a0b5d91f6daae3ef9b3b8e44c478f10d0a003dc
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-12-31 12:56:58 +0100

    Webpack. number of entries reduced, optimizatio only when deployed, compression plugin options added

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -71,23 +71,5 @@ module.exports = {
       chunkFilename: '[id].css',
     }),
     new CLEAN_WEBPACK_PLUGIN( pathsToClean , cleanOptions),
-  ],
-  optimization: {
-    minimize: true,
-    runtimeChunk: "single",
-    splitChunks: {
-        chunks: "all",
-        maxInitialRequests: Infinity,
-        minSize: 0,
-        cacheGroups: {
-            vendor: {
-                test: /[\\/]node_modules[\\/]/,
-                name(module) {
-                    const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-                    return `npm.${ packageName.replace('@', '') }`;
-                }
-            }
-        }
-    }
-  },
+  ]
 };
--- webpack/webpack.deploy.js
+++ webpack/webpack.deploy.js
@@ -10,12 +10,37 @@ const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(base, {
   output: {
-    path: DIR,
-    publicPath: '/'
+    path: DIR
   },
+  devtool: false,
   plugins: [
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-    new CompressionPlugin(),
+    new CompressionPlugin({
+        algorithm: 'gzip',
+        test: /\.js(\?.*)?$/i,
+        exclude: /server.*/,
+        deleteOriginalAssets: true
+    }),
     // new webpack.HashedModuleIdsPlugin()
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    // runtimeChunk: "single",
+    splitChunks: {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+            vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name: 'vendor',
+                chunks: 'all'
+                // name(module) {
+                //     const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                //     return `npm.${ packageName.replace('@', '') }`;
+                // }
+            }
+        }
+    }
+  }
 });
