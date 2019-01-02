4d560335599f7ea933c461d66f8ec61de079c5be
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-02 17:55:49 +0100

    Course constants.styles COURSE_HEIGHT is number

--- src/components/Course/constants.styles.ts
+++ src/components/Course/constants.styles.ts
@@ -1,5 +1,5 @@
 
-export const COURSE_HEIGHT = '200px';
+export const COURSE_HEIGHT = 225;
 export const COLUMNS = 2;
 export const SPACING_BEETWEEN_LESSONS = 2;
 export const TRANSITION_DURATION = 'complex';
