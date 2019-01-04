4afa320ee103c338d02efbef89fe948b20cce3bc
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-04 11:54:10 +0100

    MongoDB create.collection and createCourseCollection deleted. Mongoose creates collection when first document inserted

--- src/utilis/MongoDB/index.js
+++ src/utilis/MongoDB/index.js
@@ -2,18 +2,6 @@ require('dotenv').config();
 const generateCourses = require('./generate.courses');
 const generateTranslations = require('./generate.translations');
 
-async function createCoursesCollection() {
-    let fun = require('./create.collection').createCollection;
-
-    try {
-        await fun();
-        console.log('DONE');
-        return true;
-    } catch (err) {
-        throw err;
-    }
-}
-
 function insertCourses() {
     let fun = require('./insert.courses').insertCourses;
 
@@ -39,7 +27,6 @@ function insertTranslations() {
 }
 
 module.exports = {
-    createCoursesCollection,
     insertCourses,
     insertTranslations,
     generateCourses, // into the directory MongoDB/courses
