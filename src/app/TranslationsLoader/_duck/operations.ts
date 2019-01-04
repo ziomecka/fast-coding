1ea980a7603ce2c850bef967259be4b37feb7c6d
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-04 20:26:19 +0100

    TranslationsLoader First commit

---
+++ src/app/TranslationsLoader/_duck/operations.ts
@@ -0,0 +1,27 @@
+import { Dispatch } from 'redux';
+
+import { AppRoutesEnum } from '@appTypes';
+import { get } from '@app/api/';
+
+import { GetTranslationsResponseI } from './types';
+import { saveTranslations } from './actions';
+
+const _url = AppRoutesEnum.translationsGet;
+
+export const onLoadTranslations = (url: string = _url ): any => (
+    async (dispatch: Dispatch): Promise<boolean> => {
+        try {
+            let data = await get({ path: url }) as GetTranslationsResponseI;
+            let answer = await dispatch(saveTranslations(data.translations));
+            data = null; // GC
+
+            if ( answer ) {
+                answer = null; // GC
+                return true;
+            }
+        } catch (err) {}
+});
+
+export default {
+    onLoadTranslations
+};
