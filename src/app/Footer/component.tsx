cd5bee51c29d5ec3bb0ded7d943ab2bb4ab62cf8
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-06 11:13:30 +0100

    Footer OpenDialogButton for PrivacyPolicy used. Unused imports removed

--- src/app/Footer/component.tsx
+++ src/app/Footer/component.tsx
@@ -11,25 +11,19 @@ import styles from './styles';
 
 import { FooterProps } from './container';
 import Grid from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList';
-import Typography from '@material-ui/core/Typography';
+import PrivacyPolicy from '@app/PrivacyPolicy';
+
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 
-import { Link } from 'react-router-dom';
+import OpenDialogButton from '@app/OpenDialogButton';
+import { DialogsEnum } from '@app/Dialog/'
 
-import Media from 'react-media';
-import { MEDIA_DESKTOP_LG, MEDIA_DESKTOP_MD} from '@constantsStyles';
-
-import { AppRoutesEnum } from '@appTypes';
-const { privacyPolicy } = AppRoutesEnum;
+const { yes } = DialogsEnum;
 
 const FooterComponent: React.StatelessComponent< FooterProps > = props => {
     const coded = 'footerCoded';
     const author = 'footerAuthor';
-    const privacyPolicy = 'privacyPolicy';
-    const cols = 2;
-    const spacing = 4;
 
     const {
         classes: {
@@ -73,16 +67,14 @@ const FooterComponent: React.StatelessComponent< FooterProps > = props => {
                                 <Translate id={ author } />
                             </span>
                         </ListItem>
-                        <ListItem
-                            button
-                            ContainerComponent="a"
-                            ContainerProps={{
-                                // href: ''
-                            }}
-                        >
-                            <Link to={ privacyPolicy } >
-                                <Translate id={ privacyPolicy } />
-                            </Link>
+                        <ListItem>
+                            <OpenDialogButton
+                                translationId="privacyPolicy"
+                                options={{
+                                    variant: yes,
+                                    Component: PrivacyPolicy
+                                }}
+                            />
                         </ListItem>
                     </List>
                 </Grid>
