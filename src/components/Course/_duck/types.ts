3e9a95ded45fff5b1298487e196d655ee27d30cb
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-06 19:22:57 +0100

    Course withMedia used, CourseGrid added

--- src/components/Course/_duck/types.ts
+++ src/components/Course/_duck/types.ts
@@ -1,5 +1,5 @@
-// Use ComponentsContainersEnum enum in '@@components_
-// Important for components reducer
+import { MediaEnum } from '@app/Media/';
+
 export enum CourseActionsEnum {
     COMPONENTS_COURSE_UPDATE = '@@components_course/LAST_LESSON_UPDATE',
     COMPONENTS_COURSE_INITIAL_STATE_SET = '@@components_course/INITIAL_STATE_SET'
@@ -8,3 +8,5 @@ export enum CourseActionsEnum {
 export enum LessonsTypesEnum {
     review = "review"
 }
+
+export type CourseGrid = Map< MediaEnum, { cols: number, cellHeight: number }>;
