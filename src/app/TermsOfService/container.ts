03b0afc7da1884121469d513bbfc859fcaf19f25
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-09 18:57:47 +0100

    TermsOfService first commit

---
+++ src/app/TermsOfService/container.ts
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+
+import { default as TermsOfService } from './component';
+import { LocalizeState } from 'react-localize-redux';
+
+import { ApplicationState } from '@appStore';
+
+import { InfoEnum } from '@app/Info/';
+
+const mapStateToProps = (state: ApplicationState): IMapStateToProps => ({
+    localize: state.localize
+});
+
+// @ts-ignore
+const TermsOfServiceContainer = connect(mapStateToProps)(TermsOfService);
+
+export default TermsOfServiceContainer;
+
+interface IMapStateToProps {
+    localize: LocalizeState
+}
+
+export interface TermsOfServiceProps extends IMapStateToProps {
+    variant: InfoEnum;
+};
