82b123be38cf951018bf9cb3240ef05be8b619e8
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-11 15:56:08 +0100

    AppMenu fotSize for menuIconClass set for xs and md

--- src/app/AppMenu/menu.icon.class.ts
+++ src/app/AppMenu/menu.icon.class.ts
@@ -1,19 +1,23 @@
 const style = theme => {
     const {
         typography: {
-            h2: { fontSize: buttonFontSize },
+            h2: { fontSize: fontSize_SM },
+            h3: { fontSize: fontSize_XS }
         }
     } = theme;
 
     return {
         menuIconClass: {
             position: 'relative',
-            fontSize: buttonFontSize,
+            fontSize: fontSize_XS,
             padding: 0,
             marginLeft: '20px',
             '&:nth-of-type(1)': {
                 marginLeft: 0 // for Home Icon on xs screen
-            }
+            },
+            [ theme.breakpoints.up( 'sm' ) ]: {
+                fontSize: fontSize_SM,
+            },
         }
     };
 };
--- src/app/AppMenu/styles.ts
+++ src/app/AppMenu/styles.ts
@@ -9,22 +9,12 @@ import {
 import { createStyles } from '@material-ui/core/styles';
 
 const style = createStyles( theme => {
-
-    const {
-        typography: {
-            h2: { fontSize }
-        }
-    } = theme;
-
     return {
         toolbar: {
             width: 'auto', // without it takes 100% and is over the main title
             padding: 0,
             position: 'fixed',
             right: PAPER_PADDING_MAX_XS,
-            '& button': {
-                fontSize,
-            },
             height: NAV_HEIGHT_MD,
             [ theme.breakpoints.up( 'sm' ) ]: {
                 right: PAPER_PADDING_MAX_MD,
