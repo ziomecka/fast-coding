343f8d9416f71400e92c82dd09044540f5f40997
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-11 10:04:04 +0100

    Revert "webpack, tsconfig aliases from @forms, @src, @appForms"
    This reverts commit efaa85b28817c03540711cb081eda7ceb30d1c35.

--- webpack/alias.js
+++ webpack/alias.js
@@ -11,10 +11,8 @@ module.exports = {
     '@componentsTypes': ( path.resolve ( __dirname, '../src/components/_types/' ) ),
     '@viewsTypes': ( path.resolve ( __dirname, '../src/views/_types/' ) ),
     '@sharedTypes': ( path.resolve ( __dirname, '../src/shared/_types/'  )),
-    '@appForm': ( path.resolve ( __dirname, '../src/forms/Form/' )),
+    '@appForm': ( path.resolve ( __dirname, '../src/app/Form/' )),
     '@constants': ( path.resolve ( __dirname, '../src/constants' )),
     '@constantsStyles': ( path.resolve ( __dirname, '../src/theme/constants' )),
-    '@appStore': ( path.resolve ( __dirname, '../src/store' )),
-    '@forms': ( path.resolve ( __dirname, '../src/forms/' )),
-    '@src': ( path.resolve ( __dirname, '../src/' ))
+    '@appStore': ( path.resolve ( __dirname, '../src/store' ))
 };
