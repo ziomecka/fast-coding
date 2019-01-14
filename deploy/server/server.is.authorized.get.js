2451e447103cd6cd9a406c4b5ddd87ae3ff69413
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-14 23:08:48 +0100

    server.is.authorized refreshToken

--- deploy/server/server.is.authorized.get.js
+++ deploy/server/server.is.authorized.get.js
@@ -23,7 +23,7 @@ module.exports = async ( req, res ) => {
             login,
             authorizationMethod,
             displayName,
-            refreshToken: newRefreshToken
+            refreshToken
         });
     }
 };
