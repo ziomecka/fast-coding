08e6c8c410a507bc8167addddbde53f3e7a93a2f
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-16 15:41:39 +0100

    server, try catch for await added, delete cookie: domain and secure added

--- src/server/server.logout.get.js
+++ src/server/server.logout.get.js
@@ -1,31 +1,50 @@
+require('dotenv').config();
 const passwordManager = require('./PasswordManager/');
-const {
+let {
+    DOMAIN: domain,
     VERIFY_PASSWORD: { COOKIE_NAME, AUTHORIZATION_METHOD }} = require('./constants');
+
+let secure = true;
+
+if ( !process.env.NODE_ENV ){
+    domain = `localhost`;
+    secure = false;
+}
 
 module.exports = async ( req, res ) => {
     const { login, authorizationMethod } = req.session;
 
-    if  ( authorizationMethod === AUTHORIZATION_METHOD) {
-        let response = await passwordManager.logout({
-            login: login,
-            token: req.cookies[ COOKIE_NAME ]
-        });
-
-        if ( response ) {
-            req.session.authorized = false;
-
-            /** Delete cookie */
-            res.cookie( COOKIE_NAME, '', {
-                expires: new Date(Date.now()),
-                httpOnly: true,
-                secure,
-                domain
+    if  ( authorizationMethod === AUTHORIZATION_METHOD ) {
+        try {
+            let response = await passwordManager.logout({
+                login: login,
+                token: req.cookies[ COOKIE_NAME ]
             });
 
-            res.json( { result: 1, authorized: false } );
-            console.log(`Login: ${ login }, authorized with FC, logged out`);
+            if ( response ) {
+                req.session.authorized = false;
 
-            response = null; // GC
+                /** Delete cookie */
+                res.cookie( COOKIE_NAME, '', {
+                    maxAge: 1,
+                    httpOnly: true,
+                    secure,
+                    domain
+                });
+
+                res.json( { result: 1, authorized: false } );
+                console.log(`Login: ${ login }, authorized with FC, logged out`);
+
+                response = null; // GC
+            } else {
+                res.json( { result: 0, error: 'Not logged out' });
+                console.log(`Login: ${ login }, authorized with FC, not logged out. Error: ${ error }`);
+
+            }
+        } catch ( err ) {
+            const error = err.message || err.toString();
+            res.json( { result: 0, error });
+            console.log(`Login: ${ login }, authorized with FC, not logged out. Error: ${ error }`);
         }
     } else {
         req.session.authorized = false;
