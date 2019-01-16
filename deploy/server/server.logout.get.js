89e341668bbad4a0ff25f5291c2f629d17312a0c
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-16 15:19:18 +0100

    Deploy, logout, firebase

--- deploy/server/server.logout.get.js
+++ deploy/server/server.logout.get.js
@@ -1,22 +1,35 @@
 const passwordManager = require('./PasswordManager/');
 const {
-    VERIFY_PASSWORD: { COOKIE_NAME }} = require('./constants');
+    VERIFY_PASSWORD: { COOKIE_NAME, AUTHORIZATION_METHOD }} = require('./constants');
 
 module.exports = async ( req, res ) => {
-    let response = await passwordManager.logout({
-        login: req.session.login,
-        token: req.cookies[ COOKIE_NAME ]
-    });
+    const { login, authorizationMethod } = req.session;
 
-    if ( response ) {
-        const { login } = req.session;
+    if  ( authorizationMethod === AUTHORIZATION_METHOD) {
+        let response = await passwordManager.logout({
+            login: login,
+            token: req.cookies[ COOKIE_NAME ]
+        });
 
+        if ( response ) {
+            req.session.authorized = false;
+
+            /** Delete cookie */
+            res.cookie( COOKIE_NAME, '', {
+                expires: new Date(Date.now()),
+                httpOnly: true,
+                secure,
+                domain
+            });
+
+            res.json( { result: 1, authorized: false } );
+            console.log(`Login: ${ login }, authorized with FC, logged out`);
+
+            response = null; // GC
+        }
+    } else {
         req.session.authorized = false;
-
-        res.clearCookie( COOKIE_NAME );
         res.json( { result: 1, authorized: false } );
-        console.log(`Login: ${ login } logged out`);
-
-        response = null; // GC
+        console.log(`Login: ${ login }, authorized with firebase, logged out`);
     }
 };
