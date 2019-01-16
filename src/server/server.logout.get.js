c7f6cdba3b5d22b25485525424d27858eb88f2ae
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-16 10:23:58 +0100

    server, logout.get, logout via PasswordManager, if success then clearCookie

--- src/server/server.logout.get.js
+++ src/server/server.logout.get.js
@@ -1,6 +1,22 @@
-module.exports = ( req, res ) => {
-    const { login, email } = req.session;
-    req.session.authorized = false;
-    res.json( { result: 1, authorized: false } );
-    console.log(`Email: ${ email }, login: ${ login } logged out`);
+const passwordManager = require('./PasswordManager/');
+const {
+    VERIFY_PASSWORD: { COOKIE_NAME }} = require('./constants');
+
+module.exports = async ( req, res ) => {
+    let response = await passwordManager.logout({
+        login: req.session.login,
+        token: req.cookies[ COOKIE_NAME ]
+    });
+
+    if ( response ) {
+        const { login } = req.session;
+
+        req.session.authorized = false;
+
+        res.clearCookie( COOKIE_NAME );
+        res.json( { result: 1, authorized: false } );
+        console.log(`Login: ${ login } logged out`);
+
+        response = null; // GC
+    }
 };
