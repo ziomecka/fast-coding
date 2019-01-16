f312dee6f82bd9644f2b3b7311a5c3650a4cedbf
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-16 15:14:29 +0100

    server.logout verify with passwordManager only if logged with FC

--- src/server/server.logout.get.js
+++ src/server/server.logout.get.js
@@ -1,29 +1,35 @@
 const passwordManager = require('./PasswordManager/');
 const {
-    VERIFY_PASSWORD: { COOKIE_NAME }} = require('./constants');
+    VERIFY_PASSWORD: { COOKIE_NAME, AUTHORIZATION_METHOD }} = require('./constants');
 
 module.exports = async ( req, res ) => {
-    let response = await passwordManager.logout({
-        login: req.session.login,
-        token: req.cookies[ COOKIE_NAME ]
-    });
+    const { login, authorizationMethod } = req.session;
 
-    if ( response ) {
-        const { login } = req.session;
-
-        req.session.authorized = false;
-
-        /** Delete cookie */
-        res.cookie( COOKIE_NAME, '', {
-            expires: new Date(Date.now()),
-            httpOnly: true,
-            secure,
-            domain
+    if  ( authorizationMethod === AUTHORIZATION_METHOD) {
+        let response = await passwordManager.logout({
+            login: login,
+            token: req.cookies[ COOKIE_NAME ]
         });
 
-        res.json( { result: 1, authorized: false } );
-        console.log(`Login: ${ login } logged out`);
+        if ( response ) {
+            req.session.authorized = false;
 
-        response = null; // GC
+            /** Delete cookie */
+            res.cookie( COOKIE_NAME, '', {
+                expires: new Date(Date.now()),
+                httpOnly: true,
+                secure,
+                domain
+            });
+
+            res.json( { result: 1, authorized: false } );
+            console.log(`Login: ${ login }, authorized with FC, logged out`);
+
+            response = null; // GC
+        }
+    } else {
+        req.session.authorized = false;
+        res.json( { result: 1, authorized: false } );
+        console.log(`Login: ${ login }, authorized with firebase, logged out`);
     }
 };
