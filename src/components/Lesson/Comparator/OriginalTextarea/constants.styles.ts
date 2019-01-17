aa91866680e85364afb5f6003614bfcfad0c1969
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-17 12:50:11 +0100

    OriginalTextArea number of lines changed

--- src/components/Lesson/Comparator/OriginalTextarea/constants.styles.ts
+++ src/components/Lesson/Comparator/OriginalTextarea/constants.styles.ts
@@ -3,11 +3,11 @@ import { MediaEnum } from '@app/Media/';
 const { lg, md, sm, xl, xs } = MediaEnum;
 
 export const GRID = new Map( [
-    [ xs, { fontSize: '1.5rem', lettersNumber: 8, rowsNumber: 3 } ],
-    [ sm, { fontSize: '2rem', lettersNumber: 10, rowsNumber: 4 } ],
-    [ md, { fontSize: '3rem', lettersNumber: 10, rowsNumber: 4 } ],
-    [ lg, { fontSize: '3.5rem', lettersNumber: 10, rowsNumber: 4 } ],
-    [ xl, { fontSize: '3.5rem', lettersNumber: 10, rowsNumber: 4 } ],
+    [ xs, { fontSize: '1.5rem', lettersNumber: 8, rowsNumber: 2 } ],
+    [ sm, { fontSize: '2rem', lettersNumber: 10, rowsNumber: 3 } ],
+    [ md, { fontSize: '3rem', lettersNumber: 10, rowsNumber: 3 } ],
+    [ lg, { fontSize: '3.5rem', lettersNumber: 10, rowsNumber: 3 } ],
+    [ xl, { fontSize: '3.5rem', lettersNumber: 10, rowsNumber: 3 } ],
 ] );
 
 export const LETTER_MARGIN = .2;
