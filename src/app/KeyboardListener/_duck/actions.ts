e3da4ad1b6295934010ae34b565a02a6d434668e
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-17 18:15:06 +0100

    Revert "KeyboardListener, actions added"
    This reverts commit 927fb05348331154a655a903d0c1199afe0cabf9.

--- src/app/KeyboardListener/_duck/actions.ts
+++ src/app/KeyboardListener/_duck/actions.ts
@@ -1,33 +1,16 @@
-import { Action, ActionCreator } from 'redux';
+import { Action } from 'redux';
 import {
     AddListener,
-    KeyboardListenerActionsEnum,
     RemoveAllListeners,
     RemoveListener,
 } from './types';
 
-const {
-    APP_KEYBOARD_LISTENER_ADD,
-    APP_KEYBOARD_LISTENER_REMOVE,
-    APP_KEYBOARD_LISTENER_REMOVE_ALL
-} = KeyboardListenerActionsEnum;
+export const actions = {};
 
-export const addListener: ActionCreator<AddListenerAction> = ( options: AddListener ) => ( {
-    type: APP_KEYBOARD_LISTENER_ADD,
-    ...options
-} );
 
-export const removeListener: ActionCreator<RemoveListenerAction> = ( options: RemoveListener ) => ( {
-    type: APP_KEYBOARD_LISTENER_REMOVE,
-    ...options
-} );
 
-export const removeAllListeners: ActionCreator<RemoveAllListenersAction> = ( options: RemoveAllListeners ) => ( {
-    type: APP_KEYBOARD_LISTENER_REMOVE_ALL,
-    ...options
-} );
 
-export interface AddListenerAction extends AddListener, Action {}
+export interface AddListenerAction extends RemoveListener, Action {}
 export interface RemoveListenerAction extends RemoveListener, Action {}
 export interface RemoveAllListenersAction extends RemoveAllListeners, Action {}
 
