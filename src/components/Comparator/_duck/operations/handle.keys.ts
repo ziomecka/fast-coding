a845b347825b4f7722256e3088238d0c49325ac6
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-17 15:37:08 +0100

    Comparator, _duck, operations, constants moved to seperate file

--- src/components/Comparator/_duck/operations/handle.keys.ts
+++ src/components/Comparator/_duck/operations/handle.keys.ts
@@ -22,28 +22,11 @@ import {
 import history from '@shared/history';
 import { onStartLeaving } from '@components/LessonButtons/';
 
-
-// TODO - keycode differs between browsers
-/**
- * @constant {array}
- * [32] - space
- * [48, 90] - digits, letters
- * [96, 111] - numpad
- * [173, 173] - minus in Firefox
- * [186, 192] - special chars
- * [219, 222] - special chars
- */
-export const validCodes = [
-    [ 32, 32 ],
-    [ 48, 90 ],
-    [ 96, 111 ],
-    [ 173, 173 ],
-    [ 186, 192 ],
-    [ 219, 222 ],
-];
-
-export const backspace = 8;
-export const escape = 27;
+import {
+    backspace,
+    escape,
+    validCodes
+} from './constants';
 
 export const isValidCode = ( code: number ): boolean => {
     return validCodes.some( range => (
