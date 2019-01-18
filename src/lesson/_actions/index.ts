b0087f3b2aa8df02aeacc9ec3d26c96f842a3cba
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-18 16:04:19 +0100

    lesson _actions, union type for all lesson actions: TLessonActions created

--- src/lesson/_actions/index.ts
+++ src/lesson/_actions/index.ts
@@ -1,4 +1,13 @@
-
+import { LessonActions } from '@lesson/Lesson/_duck/actions';
+import { LessonButtonsActions } from '@lesson/LessonButtons/_duck/actions';
 import { LessonComparatorActions } from '@lesson/LessonComparator/';
+import { LessonStatsActions } from '@lesson/LessonStats/_duck/actions';
+import { LessonTextGeneratorActions } from '@lesson/LessonTextGenerator/_duck/actions';
 
-export type LessonActions = LessonComparatorActions;
+export type TLessonActions =
+LessonActions |
+LessonButtonsActions |
+LessonComparatorActions |
+LessonStatsActions |
+LessonTextGeneratorActions;
+
