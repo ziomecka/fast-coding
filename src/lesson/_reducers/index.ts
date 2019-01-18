6d0b02047e8c1e5717a61e3da4301b3d42b06824
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-18 16:07:38 +0100

    lesson, _reducers, combineReducers replaced with own function, dispatch onKeepState if action requires keepingState

--- src/lesson/_reducers/index.ts
+++ src/lesson/_reducers/index.ts
@@ -1,5 +1,3 @@
-import { combineReducers, Reducer } from 'redux';
-
 import {
     lessonComparatorReducer,
     INITIAL_STATE as LESSON_COMPARATOR_INITIAL_STATE
@@ -25,7 +23,16 @@ import {
     lessonStatsReducer,
 } from '@lesson/LessonStats/';
 
-import { ILessonState } from '../_types/index';
+import { ILessonState, } from '../_types/index';
+import { TLessonActions } from '../_actions/';
+import { Reducer } from 'redux';
+import { onKeepState } from '../Lesson/_duck/operations';
+
+import { stateKeepActions as lessonComparatorStateKeepActions } from '@lesson/LessonComparator/_duck/actions';
+import { stateKeepActions as lessonStateKeepActions } from '@lesson/Lesson/_duck/actions';
+import { stateKeepActions as lessonStatsKeepActions } from '@lesson/LessonStats/_duck/actions';
+
+import store from '@appStore';
 
 export const INITIAL_STATE = {
     lessonComparator: { ...LESSON_COMPARATOR_INITIAL_STATE },
@@ -35,13 +42,28 @@ export const INITIAL_STATE = {
     lessonStats: { ...LESSON_STATS_INITIAL_STATE }
 };
 
-const reducer: Reducer<ILessonState> = combineReducers( {
-    lesson: lessonReducer,
-    lessonComparator: lessonComparatorReducer,
-    lessonTextGenerator: lessonTextGeneratorReducer,
-    lessonButtons: lessonButtonsReducer,
-    lessonStats: lessonStatsReducer
-} );
+/** Get actions that require keeping the state */
+const keepStateActions = [
+    ...lessonComparatorStateKeepActions,
+    ...lessonStateKeepActions,
+    ...lessonStatsKeepActions
+];
+
+const reducer: Reducer<ILessonState, TLessonActions> = ( state = INITIAL_STATE, action ) => {
+    /** Keep lesson state if the action requires it */
+    if ( keepStateActions.indexOf( action.type ) !== -1 ) {
+        store.dispatch( onKeepState( state ) );
+    }
+
+    return {
+        lesson: lessonReducer( state.lesson, action ),
+        // @ts-ignore
+        lessonComparator: lessonComparatorReducer( state.lessonComparator, action ),
+        lessonTextGenerator: lessonTextGeneratorReducer( state.lessonTextGenerator, action ),
+        lessonButtons: lessonButtonsReducer( state.lessonButtons, action ),
+        lessonStats: lessonStatsReducer( state.lessonStats, action )
+    };
+};
 
 export { reducer as lessonReducer };
 
