e69407055bf5dc8d5345effc8f30f743bff69347
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-18 16:11:23 +0100

    LocalStorage, only one enum for lesson, lesson state will be stored under one item in localStorage

--- src/app/LocalStorage/_duck/types.ts
+++ src/app/LocalStorage/_duck/types.ts
@@ -4,7 +4,5 @@ export type LocalStorageDataTypes = ICourseData;
 
 export enum LocalStorageItemEnum {
     courses = 'COURSES',
-    lessonComparator = 'LESSON_COMPARATOR',
-    lesson = 'LESSON',
-    lessonStats = 'LESSON_STATS'
+    lesson = 'LESSON'
 }
