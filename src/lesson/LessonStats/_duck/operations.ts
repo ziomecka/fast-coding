ea95238207edc465319d50462ed05862c7480430
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-18 17:20:36 +0100

    LessonStats dispatch onKeepsState, no arguments - as one state for lesson is stored in localStorage

--- src/lesson/LessonStats/_duck/operations.ts
+++ src/lesson/LessonStats/_duck/operations.ts
@@ -1,30 +1,25 @@
 import { Dispatch } from 'redux';
 import { startTimer, stopTimer, pauseLessonStats, unpauseLessonStats } from './actions';
 import { onKeepState } from '@lesson/Lesson/';
-import { LocalStorageItemEnum } from '@appTypes';
-import { LessonContainersEnum } from '@lessonTypes';
-
-const { lessonStats: localStorageItem } = LocalStorageItemEnum;
-const { lessonStats: container } = LessonContainersEnum;
 
 export const onStartTimer = (): any => ( dispatch: Dispatch ) => {
     dispatch( startTimer() );
-    return dispatch( onKeepState( { container, localStorageItem } ) );
+    return dispatch( onKeepState() );
 };
 
 export const onStopTimer = (): any => ( dispatch: Dispatch ) => {
     dispatch( stopTimer() );
-    return dispatch( onKeepState( { container, localStorageItem } ) );
+    return dispatch( onKeepState() );
 };
 
 export const onPauseTimer = (): any => ( dispatch: Dispatch ) => {
     dispatch( pauseLessonStats() );
-    return dispatch( onKeepState( { container, localStorageItem } ) );
+    return dispatch( onKeepState() );
 };
 
 export const onUnpauseTimer = (): any => ( dispatch: Dispatch ) => {
     dispatch( unpauseLessonStats( localStorageItem ) );
-    return dispatch( onKeepState( { container, localStorageItem } ) );
+    return dispatch( onKeepState() );
 };
 
 export default {};
