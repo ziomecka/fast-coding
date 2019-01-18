17af0b08da291977237c44afb0af35f444736809
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-18 17:17:04 +0100

    Revert "LessonStats, do not dispatch onKeepState in operations"
    This reverts commit 33477cbb97994982ae65cfa4a1298aac6af6f4b9.

--- src/lesson/LessonStats/_duck/operations.ts
+++ src/lesson/LessonStats/_duck/operations.ts
@@ -1,20 +1,30 @@
 import { Dispatch } from 'redux';
 import { startTimer, stopTimer, pauseLessonStats, unpauseLessonStats } from './actions';
+import { onKeepState } from '@lesson/Lesson/';
+import { LocalStorageItemEnum } from '@appTypes';
+import { LessonContainersEnum } from '@lessonTypes';
 
-export const onStartTimer = (): any => ( dispatch: Dispatch ) => (
-    dispatch( startTimer() )
-);
+const { lessonStats: localStorageItem } = LocalStorageItemEnum;
+const { lessonStats: container } = LessonContainersEnum;
 
-export const onStopTimer = (): any => ( dispatch: Dispatch ) => (
-    dispatch( stopTimer() )
-);
+export const onStartTimer = (): any => ( dispatch: Dispatch ) => {
+    dispatch( startTimer() );
+    return dispatch( onKeepState( { container, localStorageItem } ) );
+};
 
-export const onPauseTimer = (): any => ( dispatch: Dispatch ) => (
-    dispatch( pauseLessonStats() )
-);
+export const onStopTimer = (): any => ( dispatch: Dispatch ) => {
+    dispatch( stopTimer() );
+    return dispatch( onKeepState( { container, localStorageItem } ) );
+};
 
-export const onUnpauseTimer = (): any => ( dispatch: Dispatch ) => (
-    dispatch( unpauseLessonStats() )
-);
+export const onPauseTimer = (): any => ( dispatch: Dispatch ) => {
+    dispatch( pauseLessonStats() );
+    return dispatch( onKeepState( { container, localStorageItem } ) );
+};
+
+export const onUnpauseTimer = (): any => ( dispatch: Dispatch ) => {
+    dispatch( unpauseLessonStats( localStorageItem ) );
+    return dispatch( onKeepState( { container, localStorageItem } ) );
+};
 
 export default {};
