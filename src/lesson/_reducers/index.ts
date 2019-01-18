f952ca1a0a4371a779131dc2cb286f0c4637a02f
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-18 17:16:10 +0100

    Revert "lesson, _reducers, combineReducers replaced with own function, dispatch onKeepState if action requires keepingState"
    This reverts commit 415b610bef8a724b09515c5aa8284e2728549709.

--- src/lesson/_reducers/index.ts
+++ src/lesson/_reducers/index.ts
@@ -1,3 +1,5 @@
+import { combineReducers, Reducer } from 'redux';
+
 import {
     lessonComparatorReducer,
     INITIAL_STATE as LESSON_COMPARATOR_INITIAL_STATE
@@ -23,16 +25,7 @@ import {
     lessonStatsReducer,
 } from '@lesson/LessonStats/';
 
-import { ILessonState, } from '../_types/index';
-import { TLessonActions } from '../_actions/';
-import { Reducer } from 'redux';
-import { onKeepState } from '../Lesson/_duck/operations';
-
-import { stateKeepActions as lessonComparatorStateKeepActions } from '@lesson/LessonComparator/_duck/actions';
-import { stateKeepActions as lessonStateKeepActions } from '@lesson/Lesson/_duck/actions';
-import { stateKeepActions as lessonStatsKeepActions } from '@lesson/LessonStats/_duck/actions';
-
-import store from '@appStore';
+import { ILessonState } from '../_types/index';
 
 export const INITIAL_STATE = {
     lessonComparator: { ...LESSON_COMPARATOR_INITIAL_STATE },
@@ -42,28 +35,13 @@ export const INITIAL_STATE = {
     lessonStats: { ...LESSON_STATS_INITIAL_STATE }
 };
 
-/** Get actions that require keeping the state */
-const keepStateActions = [
-    ...lessonComparatorStateKeepActions,
-    ...lessonStateKeepActions,
-    ...lessonStatsKeepActions
-];
-
-const reducer: Reducer<ILessonState, TLessonActions> = ( state = INITIAL_STATE, action ) => {
-    /** Keep lesson state if the action requires it */
-    if ( keepStateActions.indexOf( action.type ) !== -1 ) {
-        store.dispatch( onKeepState( state ) );
-    }
-
-    return {
-        lesson: lessonReducer( state.lesson, action ),
-        // @ts-ignore
-        lessonComparator: lessonComparatorReducer( state.lessonComparator, action ),
-        lessonTextGenerator: lessonTextGeneratorReducer( state.lessonTextGenerator, action ),
-        lessonButtons: lessonButtonsReducer( state.lessonButtons, action ),
-        lessonStats: lessonStatsReducer( state.lessonStats, action )
-    };
-};
+const reducer: Reducer<ILessonState> = combineReducers( {
+    lesson: lessonReducer,
+    lessonComparator: lessonComparatorReducer,
+    lessonTextGenerator: lessonTextGeneratorReducer,
+    lessonButtons: lessonButtonsReducer,
+    lessonStats: lessonStatsReducer
+} );
 
 export { reducer as lessonReducer };
 
