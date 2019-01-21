24b012c86482d6e3253a9d2084bd5675323c460d
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-21 18:27:50 +0100

    state.restore, if lesson is paused then dispatch onPauseLesson, so the paused lesson listeners work

--- src/lesson/_operations/state.restore.ts
+++ src/lesson/_operations/state.restore.ts
@@ -4,10 +4,20 @@ import { LocalStorageItemEnum } from '@appTypes';
 import { localStorageGetItem } from '@app/LocalStorage/_duck/operations';
 import { restoreState } from '@lesson/_actions/';
 
+import { onPauseLesson } from './life';
+import { pausedLessonListener } from '@lesson/LessonComparator/';
+
 export const onRestoreState = (): any => (
-    async ( dispatch: Dispatch ): Promise<Action> => (
-        dispatch( restoreState( localStorageGetItem( LocalStorageItemEnum.lesson ) ) )
-    )
+    async ( dispatch: Dispatch ): Promise<Action> => {
+        const { paused } = localStorageGetItem( LocalStorageItemEnum.lesson );
+
+        if ( paused ) {
+            /** If paused lesson is refreshed then the pausedLessonListener will work */
+            dispatch( onPauseLesson( pausedLessonListener ) );
+        }
+
+        return dispatch( restoreState( localStorageGetItem( LocalStorageItemEnum.lesson ) ) );
+    }
 );
 
 export default { onRestoreState };
