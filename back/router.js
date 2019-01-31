696955487d0f4b6a0aac41df8a2dea4dde764b5e
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-31 11:12:09 +0100

    server, router into seperate file

---
+++ back/router.js
@@ -0,0 +1,52 @@
+const {
+    ROUTES: {
+        CHANGE_PASSWORD,
+        IS_AUTHORIZED,
+        LESSONS_GET,
+        LOGIN_FIREBASE,
+        LOGIN_LOG,
+        LOGOUT,
+        NEW_PASSWORD,
+        NEW_USER_SET,
+        REMIND_PASSWORD,
+        TRANSLATIONS_GET,
+    },
+} = require('./constants');
+
+const path = require('path');
+const router = require('express').Router();
+
+const serverChangePassword = require('./server.change.password');
+const serverFavicon = require('./server.favicon');
+const serverIsAuthorized = require('./server.is.authorized.get');
+const serverLessonsGet = require('./server.lessons.get');
+const serverLoginFirebase = require('./server.login.firebase.post');
+const serverLoginLog = require('./server.login.log');
+const serverLogoutGet = require('./server.logout.get');
+const serverNewPassword = require('./server.new.password');
+const serverNewUserSet = require('./server.newuser.set');
+const serverRemindPassword = require('./server.remind.password');
+const serverTranslationsGet = require('./server.translations.get');
+
+const ROOT = path.resolve(__dirname, './');
+
+const HTML_PATH = !process.env.NODE_ENV
+    ? path.resolve(ROOT, '../_bundleFront/index.html')
+    : path.resolve(ROOT, '../_deploy/index.html');
+
+router.get( '/favicon.ico', serverFavicon );
+router.get( IS_AUTHORIZED, serverIsAuthorized );
+router.get( LESSONS_GET, serverLessonsGet );
+router.get ( LOGOUT, serverLogoutGet );
+router.get( TRANSLATIONS_GET, serverTranslationsGet );
+router.get( '/*.js', serverJavascript );
+router.get('*', (req, res) => res.sendFile( HTML_PATH, { root: ROOT } ) );
+
+router.post( CHANGE_PASSWORD, serverChangePassword );
+router.post ( LOGIN_FIREBASE, serverLoginFirebase );
+router.post( LOGIN_LOG, serverLoginLog );
+router.post( NEW_PASSWORD, serverNewPassword );
+router.post( NEW_USER_SET, serverNewUserSet );
+router.post( REMIND_PASSWORD, serverRemindPassword );
+
+module.exports = router;
