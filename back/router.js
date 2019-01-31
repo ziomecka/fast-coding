068c741474b2fd2c44b309393a90dd3527a3f437
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-31 11:12:09 +0100

    server, router into seperate file

---
+++ back/router.js
@@ -0,0 +1,45 @@
+const {
+    ROUTES: {
+        CHANGE_PASSWORD,
+        IS_AUTHORIZED,
+        LESSONS_GET,
+        LOGIN_FIREBASE,
+        LOGIN_LOG,
+        LOGOUT,
+        NEW_PASSWORD,
+        NEW_USER_SET,
+        REMIND_PASSWORD,
+        TRANSLATIONS_GET,
+    },
+} = require('./constants');
+
+const router = require('express').Router();
+
+const serverChangePassword = require('./server.change.password');
+const serverFavicon = require('./server.favicon');
+const serverIsAuthorized = require('./server.is.authorized.get');
+const serverLessonsGet = require('./server.lessons.get');
+const serverLoginFirebase = require('./server.login.firebase.post');
+const serverLoginLog = require('./server.login.log');
+const serverLogoutGet = require('./server.logout.get');
+const serverNewPassword = require('./server.new.password');
+const serverNewUserSet = require('./server.newuser.set');
+const serverRemindPassword = require('./server.remind.password');
+const serverSideRendering = require('./server.side.rendering');
+const serverTranslationsGet = require('./server.translations.get');
+
+router.get( '/favicon.ico', serverFavicon );
+router.get( IS_AUTHORIZED, serverIsAuthorized );
+router.get( LESSONS_GET, serverLessonsGet );
+router.get ( LOGOUT, serverLogoutGet );
+router.get( TRANSLATIONS_GET, serverTranslationsGet );
+router.get('*', serverSideRendering );
+
+router.post( CHANGE_PASSWORD, serverChangePassword );
+router.post ( LOGIN_FIREBASE, serverLoginFirebase );
+router.post( LOGIN_LOG, serverLoginLog );
+router.post( NEW_PASSWORD, serverNewPassword );
+router.post( NEW_USER_SET, serverNewUserSet );
+router.post( REMIND_PASSWORD, serverRemindPassword );
+
+module.exports = router;
--- back/server.js
+++ back/server.js
@@ -13,33 +13,11 @@ const constants = require('./constants');
 const getSession = require('./server.session');
 
 const serverCors = require('./server.cors');
-const serverNewUserSet = require('./server.newuser.set');
-const serverLoginLog = require('./server.login.log');
-const serverLessonsGet = require('./server.lessons.get');
-const serverChangePassword = require('./server.change.password');
-const serverFavicon = require('./server.favicon');
-const serverRemindPassword = require('./server.remind.password');
-const serverNewPassword = require('./server.new.password');
-const serverTranslationsGet = require('./server.translations.get');
-const serverIsAuthorized = require('./server.is.authorized.get');
-const serverLoginFirebase = require('./server.login.firebase.post');
-const serverLogOutGet = require('./server.logout.get');
-const serverSideRendering = require('./server.side.rendering');
+
+const router = require('./router');
 
 const {
     PORT: _PORT,
-    ROUTES: {
-        LESSONS_GET,
-        NEW_USER_SET,
-        LOGIN_LOG,
-        CHANGE_PASSWORD,
-        REMIND_PASSWORD,
-        NEW_PASSWORD,
-        TRANSLATIONS_GET,
-        IS_AUTHORIZED,
-        LOGIN_FIREBASE,
-        LOGOUT
-    },
     SESSION: { ROUTES }
 } = constants;
 
@@ -112,40 +90,7 @@ app.use( express.json() );
 app.use( express.urlencoded({ extended: false }) );
 app.use( cookieParser() );
 app.use( ROUTES, getSession() );
-
-
-/** Check if authorized */
-app.get( IS_AUTHORIZED, serverIsAuthorized );
-
-/** Get lessons */
-app.get( LESSONS_GET, serverLessonsGet );
-
-/** Get translations */
-app.get( TRANSLATIONS_GET, serverTranslationsGet );
-
-/** Set newuser */
-app.post( NEW_USER_SET, serverNewUserSet );
-
-/** Log user */
-app.post( LOGIN_LOG, serverLoginLog );
-
-/** Log with firebase */
-app.post ( LOGIN_FIREBASE, serverLoginFirebase );
-
-/** Log out */
-app.get ( LOGOUT, serverLogOutGet );
-
-/** Change password */
-app.post( CHANGE_PASSWORD, serverChangePassword );
-
-/** Remind password */
-app.post( REMIND_PASSWORD, serverRemindPassword );
-
-/** New password */
-app.post( NEW_PASSWORD, serverNewPassword );
-
-// app.get('*', (req, res) => res.sendFile(HTML_PATH, { root: path.resolve(ROOT, '../') }));
-app.get( '/favicon.ico', serverFavicon );
-app.get('*', serverSideRendering );
+app.use ( '/', router );
 
 server.listen(PORT, console.log(`Listening on ${ PORT }`));
+
