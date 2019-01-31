22c23376df3836977e28b23566fe407b0a9a1ac8
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-01-31 10:50:03 +0100

    server app.get for favicon.ico

--- back/server.js
+++ back/server.js
@@ -17,6 +17,7 @@ const serverNewUserSet = require('./server.newuser.set');
 const serverLoginLog = require('./server.login.log');
 const serverLessonsGet = require('./server.lessons.get');
 const serverChangePassword = require('./server.change.password');
+const serverFavicon = require('./server.favicon');
 const serverRemindPassword = require('./server.remind.password');
 const serverNewPassword = require('./server.new.password');
 const serverTranslationsGet = require('./server.translations.get');
@@ -144,6 +145,7 @@ app.post( REMIND_PASSWORD, serverRemindPassword );
 app.post( NEW_PASSWORD, serverNewPassword );
 
 // app.get('*', (req, res) => res.sendFile(HTML_PATH, { root: path.resolve(ROOT, '../') }));
+app.get( '/favicon.ico', serverFavicon );
 app.get('*', serverSideRendering );
 
 server.listen(PORT, console.log(`Listening on ${ PORT }`));
